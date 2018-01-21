1d8369cd6c1a7327325a9bdb1db65722fa167aa5
zfudge <zacheryfudge@gmail.com>
2018-01-20 22:07:08 -0700

    refactored description, console log

--- problem5.js
+++ problem5.js
@@ -1,11 +1,9 @@
-/* 
-The sum of the squares of the first ten natural numbers is,
-1^2 + 2^2 + ... + 10^2 = 385
-The square of the sum of the first ten natural numbers is,
-(1 + 2 + ... + 10)^2 = 55^2 = 3025
-Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
-Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-*/
+/* 	The sum of the squares of the first ten natural numbers is,
+		1^2 + 2^2 + ... + 10^2 = 385
+	The square of the sum of the first ten natural numbers is,
+		(1 + 2 + ... + 10)^2 = 55^2 = 3025
+	Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+	Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.	*/
 
 let sumOfSquares = 0;
 let squareSum = 0;
@@ -16,5 +14,6 @@ for (let i= 1; i <= 100; i++) {
 }
 
 squareSum *= squareSum;
+squareSum -= sumOfSquares;
 
-console.log(`The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is ${squareSum - sumOfSquares}.`);
+console.log(`The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is ${squareSum}.`);
