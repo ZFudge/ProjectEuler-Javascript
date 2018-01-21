f72b5e9b1b31768ec2b3ebd9e2fd98a722aa10ea
zfudge <zacheryfudge@gmail.com>
2018-01-21 07:58:50 -0700

    refactored description, output

--- problem2.js
+++ problem2.js
@@ -1,5 +1,7 @@
-// The prime factors of 13195 are 5, 7, 13 and 29.
-// What is the largest prime factor of the number 600851475143 ?
+/*
+	The prime factors of 13195 are 5, 7, 13 and 29.
+	What is the largest prime factor of the number 600851475143 ?
+*/
 
 function isPrime(num) {
 	for (let i = 2; i <= (num**0.5); i++) {
@@ -13,7 +15,7 @@ const numSqRt = Math.ceil(600851475143 ** 0.5);
 
 for (let i = numSqRt; i >= 2; i--) {
 	if (num % i == 0 && isPrime(i)) {
-		console.log(`The largest prime factor of ${num} is ${i}`);
+		console.log(`The largest prime factor of ${num} is ${i}.`);
 		break;
 	}
 }
